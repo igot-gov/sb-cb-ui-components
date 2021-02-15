5a5fed1210bcd61dee2b4d75900b1060450d5ec2
Keyur Rathod <keyur.rathod84@gmail.com>
2021-02-15 17:35:43 +0530

    removed dist folder and added gitignore

--- sb-cb-ui-components/dist/lib-components/esm5/public-api.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-/*
- * Public API Surface of lib-components
- */
-export { AvatarPhotoModule } from './lib/avatar-photo/avatar-photo.module';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljLWFwaS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL2xpYi1jb21wb25lbnRzLyIsInNvdXJjZXMiOlsicHVibGljLWFwaS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBSUEsa0NBQWMsd0NBQXdDLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKlxuICogUHVibGljIEFQSSBTdXJmYWNlIG9mIGxpYi1jb21wb25lbnRzXG4gKi9cblxuZXhwb3J0ICogZnJvbSAnLi9saWIvYXZhdGFyLXBob3RvL2F2YXRhci1waG90by5tb2R1bGUnO1xuIl19
