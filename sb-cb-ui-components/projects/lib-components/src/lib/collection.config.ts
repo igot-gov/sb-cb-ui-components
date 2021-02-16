b620e3de34e38ab3396e68b123c1cf05287d8ffe
Amit Kumar <amit.sengar@tarento.com>
2021-02-16 12:17:29 +0530

    initial version

--- sb-cb-ui-components/projects/lib-components/src/lib/collection.config.ts
+++
@@ -1,6 +0,0 @@
-export const ROOT_WIDGET_CONFIG = {
-  avatarPhoto: {
-    _type: 'avatarPhoto',
-    avatarPhoto: 'avatarPhoto',
-  }
-}
--- sb-cb-ui-components/projects/lib-components/src/public-api.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of lib-components
- */
-
-export * from './lib/avatar-photo/avatar-photo.module';
