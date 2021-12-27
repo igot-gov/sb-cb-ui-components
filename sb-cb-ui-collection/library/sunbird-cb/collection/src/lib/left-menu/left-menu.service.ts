bbe7276a7370ca25bfb1a774dc971d493e22d1ab
Jenifer Ramsingh <jeniferramsingh@gmail.com>
2021-12-27 19:21:02 +0530

    library version got updated 1.0.8

---
+++ sb-cb-ui-collection/library/sunbird-cb/collection/src/lib/left-menu/left-menu.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LeftMenuService {
+  private subject = new Subject<any>();
+
+  sendMessage(message: string) {
+      this.subject.next({ text: message });
+  }
+
+  clearMessages() {
+      this.subject.next();
+  }
+
+  onMessage(): Observable<any> {
+      return this.subject.asObservable();
+  }
+  
+}
